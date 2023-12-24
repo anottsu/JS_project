aa0ea8374901825d0b028b80aef102d03ba8d965
anottsu <anottsu@gmail.com>
2023-12-24 00:18:57 +0000

    adicionado arq variaveis

--- operadores.js
+++ operadores.js
@@ -1,4 +1,21 @@
+//VALOR BOLEANO
 let a=1, b=2;
 const nome = "anottsu";
-console.log(a === b); // COMPARA A COM B O RETORNO É FALSE
+console.log("A e B tem valores iguais? ", a === b); // O VALOR BOLEANO RETORNA TRUE OR FALSE
+//COMPARANDO OS VALORES PELO OPERADOR ===
+//SE USARMOS == ELE IRÁ COMPARAR TRUE OR FALSE ASSUMINDO 0 TRUE E 1 FALSE
+console.log(null == undefined); // true NESSE CASO ELE ENCARA OS DOIS COMO VAZIO
+console.log(null === undefined); // false NESSE CASO ELE É MAIS ESPECIFICO DIFERENCIANDO
+
+//NULL: VARIAVEL DECLARADA E INICIALIZADA MAS SEM VALOR. GOSTO DE PENSAR COMO VAZIO
+//UNDEFINED: VARIAVEL DECLARADA MAS NÃO INICIALIZADA
+
+console.log("_____________________________________________________________________ ");
+//OPERADORES >= , <= OU == (MAIOR IGUAL, MENOR IGUAL OU IGUAL) PODEM SER APLICADOS TAMBÉM.
+console.log("A é B maior ou igual a B? ", a >= b); // false 
+console.log("A é B menor ou igual a B? ", a <= b); // true
+console.log("A é B maior ou igual a B? ", a == b); // false
+
+
 console.log(`O nome ${nome.replace(/\b\w/g, (nome)=> nome.toUpperCase())} possui ${nome.length} caracteres`);
+
---
+++ variaveis.js
@@ -0,0 +1,27 @@
+//EXISTEM 3 TIPOS DE VARIAVEIS VAR, LET E CONST
+
+//VAR : Pode ser usada primeiro e depois declarada, mas não é uma boa prática
+
+// var b =2,h =3;
+// area = b*h;
+// console.log(area)
+
+// var area;
+console.log("_____________________________________________________________")
+
+//LET : Deve ser declarada para ser usada,pode ser reutilizada. É a recomendável no JS
+// let b =2,h =3;
+// let area = b*h;
+// console.log(area)
+
+console.log("_____________________________________________________________")
+
+//CONST : Deve ser declarada para ser usada, não pode ser reutilizada
+const b =2,h =3;
+let area = b*h;
+area = area + 4;
+console.log(area)
+
+console.log("_____________________________________________________________")
+
+
